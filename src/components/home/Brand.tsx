5f67377be8657a59e42f1a8320136ab42eed1b04
Adeshina Favour Adekunle <53022031+Favour-dgreat@users.noreply.github.com>
2024-05-07 14:36:56 +0100

    fixes

--- src/components/home/Brand.tsx
+++ src/components/home/Brand.tsx
@@ -1,57 +1,5 @@
-import Slider from "react-slick";
 
-const brand_data: string[] = [
-	"/assets/img/brand/brand_img01.png",
-	"/assets/img/brand/brand_img02.png",
-	"/assets/img/brand/brand_img03.png",
-	"/assets/img/brand/brand_img04.png",
-	"/assets/img/brand/brand_img05.png",
-	"/assets/img/brand/brand_img06.png",
-	"/assets/img/brand/brand_img07.png",
-];
 
-const settings = {
-   dots: false,
-	infinite: true,
-	speed: 1000,
-	autoplay: true,
-	arrows: false,
-	slidesToShow: 7,
-	slidesToScroll: 2,
-	responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 5,
-				slidesToScroll: 1,
-				infinite: true,
-			}
-		},
-		{
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 4,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 767,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1,
-				arrows: false,
-			}
-		},
-		{
-			breakpoint: 575,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 1,
-				arrows: false,
-			}
-		},
-	]
-}
 
 const Brand = () => {
    return (
